0ef1484e3a9be9af338c64e0757a71b26a280fb7
girgisshihataa <50370920+girgisshihataa@users.noreply.github.com>
2020-04-06 11:58:59 -0500

    Girgis first push of individual pages navigated to by a nav bar

---
+++ omam/src/Author.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Author(){
+    return(
+        <div>
+            <h1>Welcome to the Author's site and his argument!</h1>
+        </div>
+    )
+}
+
+export default Author;
