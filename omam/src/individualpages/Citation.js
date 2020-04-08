504849020004b1c1d82614368502d41ff2e3dc7c
girgisshihataa <50370920+girgisshihataa@users.noreply.github.com>
2020-04-08 17:43:29 -0500

    Added back to home button

--- omam/src/individualpages/Citation.js
+++ omam/src/individualpages/Citation.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Button } from '../Components/Buttons'
+import {Link} from 'react-router-dom'
 
 function Citation(){
     return(
         <div>
+        <Link to ='/'>
+          <Button
+            onClick = {() => {
+              console.log("You clicked me");
+            }}
+            type="button"
+            buttonStyle="btn--home"
+            buttonSize="btn--large"
+            >HOME
+          </Button>
+      </Link>
             <h1>Welcome to the website's citations!</h1>
         </div>
     )
--- omam/src/individualpages/History.js
+++ omam/src/individualpages/History.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Button } from '../Components/Buttons'
+import {Link} from 'react-router-dom'
 
 function History(){
     return(
         <div>
+        <Link to ='/'>
+          <Button
+            onClick = {() => {
+              console.log("You clicked me");
+            }}
+            type="button"
+            buttonStyle="btn--home"
+            buttonSize="btn--large"
+            >HOME
+          </Button>
+      </Link>
             <h1>Behind the scenes!</h1>
         </div>
     )
